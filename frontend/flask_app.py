0007fa6cfe7ffba947ef6d0f4595b341cb7dc70d
LRoescher <s5602199@msx.tu-dresden.de>
2021-12-20 16:49:20 +0100

    password login, requiered to see the app

--- frontend/flask_app.py
+++ frontend/flask_app.py
@@ -1,14 +1,33 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
+
 app = Flask(__name__, template_folder='./templates/')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return pages("main.html")
+    else:
+        flash('wrong password!')
+        return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
 
 @app.route("/<string:name>/")
-def hello(name):
-    return render_template(name)
+def pages(name):
+    if  session['logged_in']:
+        return render_template(name)
+    else:
+        return index()
+
 
 
 if __name__ == "__main__":
