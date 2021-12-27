dbde3a4111975294a6e2b555585f9bfa1caff7ec
skerberus <dumm_oder_was@gmx.de>
2021-12-27 02:46:40 +0100

    Update Layout, add blueprints, implement analyse and person data view, improve login

---
+++ Frontend/blueprint_person.py
@@ -0,0 +1,86 @@
+from flask import Blueprint, render_template, redirect, request, url_for, flash
+from Backend.interface import PatientId, Interface, PatientData
+from Backend.example_interface import Example
+from typing import Optional
+from FlashMessageTypes import FlashMessageTypes
+
+controller: Interface = Example()
+
+person_data = Blueprint("person_data", __name__)
+
+
+@person_data.context_processor
+def py_types():
+    return {
+        "int": int,
+        "bool": bool,
+        "str": str,
+        "float": float
+    }
+
+
+@person_data.route("/")
+def get_empty_patient_data():
+    # ToDo: remove id from PatientData
+    data = PatientData(id=-1, age=0, name="Tom", hasCovid=False, hasFever=False)
+    return render_template("person_data.html", patient_data=data, annotations=PatientData.__annotations__)
+
+
+@person_data.route("/<int:patient_id>")
+def get_patient_data(patient_id: PatientId):
+    # ToDo: translate to german
+    return render_template("person_data.html",
+                           patient_id=patient_id,
+                           patient_data=controller.get_patient_data(patient_id),
+                           annotations=PatientData.__annotations__)
+
+
+def get_patient_data_from_request() -> Optional[PatientData]:
+    patient_data: PatientData = {}
+    for key, data_type in PatientData.__annotations__.items():
+        if key in request.form:
+            if data_type is not bool:
+                patient_data[key] = request.form[key]
+            else:
+                patient_data[key] = True
+        elif key not in request.form and data_type is bool:
+            patient_data[key] = False
+
+        else:
+            return None
+    return patient_data
+
+
+@person_data.route("/save", methods=['POST'])
+def add_patient_data():
+    patient_data = get_patient_data_from_request()
+
+    if patient_data is None:
+        flash("Adding a new Patient failed.", FlashMessageTypes.FAILURE.value)
+        return redirect("/")
+
+    patient_id = controller.add_patient(patient_data)
+
+    if patient_id:
+        flash(f"Patient {patient_data['name']} successfully added.", FlashMessageTypes.SUCCESS.value)
+        return redirect(url_for('.get_patient_data', patient_id=patient_id))
+    else:
+        flash(f"Adding Patient {patient_data['name']} failed.", FlashMessageTypes.FAILURE.value)
+        return redirect(url_for(".get_empty_patient_data"))
+
+
+@person_data.route("/<int:patient_id>/save", methods=['POST'])
+def update_patient_data(patient_id: PatientId):
+    patient_data = get_patient_data_from_request()
+
+    if patient_data is None:
+        flash(f"Updating Patient with id:({patient_id}) failed.", FlashMessageTypes.FAILURE.value)
+        return redirect(url_for(".get_patient_data", patient_id=patient_id))
+
+    if controller.update_patient(patient_id, patient_data):
+        flash(f"Patient {patient_data['name']} successfully added.", FlashMessageTypes.SUCCESS.value)
+        return redirect(url_for('.get_patient_data', patient_id=patient_id))
+    else:
+        flash(f"Updating Patient (id: {patient_id}) {patient_data['name']} failed.", FlashMessageTypes.FAILURE.value)
+        return  redirect(url_for(".get_patient_data", patient_id=patient_id))
+
--- Frontend/flask_app.py
+++ Frontend/flask_app.py
@@ -1,6 +1,11 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for, g
-from Backend.interface import PatientId, Interface
-from typing import Callable
+from flask import Flask, flash, redirect, render_template, url_for, send_from_directory
+from datetime import timedelta
+from Backend.interface import Interface
+from Frontend.FlashMessageTypes import FlashMessageTypes
+from blueprint_person import person_data
+from blueprint_login import access_control
+from blueprint_results import results
+import os
 
 # ToDo: remove later
 from Backend.example_interface import Example
@@ -8,89 +13,39 @@ from Backend.example_interface import Example
 controller: Interface = Example()
 
 app = Flask(__name__, template_folder='./templates/')
+app.permanent_session_lifetime = timedelta(minutes=5)   # 5 min auto logout
+
+# blueprints
+app.register_blueprint(access_control)
+app.register_blueprint(person_data, url_prefix="/person_data")
+app.register_blueprint(results, url_prefix="/results")
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.before_request
-def check_login():
-    if request.endpoint in ["login", "do_admin_login", "logout"]:
-        return None
-
-    if "logged_in" not in session or not session["logged_in"]:
-        return redirect("login")
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/")
 def main():
-    return redirect(url_for("pages", name="main.html"))
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-        return redirect(url_for("pages", name="main.html"))
-    else:
-        flash('wrong password!')
-        return login()
-
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return redirect("login")
+    return render_template("main.html", db_empty=controller.is_db_empty())
 
 
 @app.route("/<string:name>/")
 def pages(name):
+    # ToDo: remove
     return render_template(name)
 
 
-@app.route("/analysis")
-def get_analysis():
-    # ToDo: login
-    return render_template("analysis.html", analysis_data=controller.analysis_data)
-
-
-@app.route("/person_data")
-def get_empty_person_data():
-    #ToDo: implement
-    pass
-
-
-@app.route("/person_data/<int:patient_id>")
-def get_person_data(patient_id: PatientId):
-    #ToDo: implement
-    pass
-
-
-@app.route("/person_data/save")
-def add_person_date():
-    #ToDo: implement
-    pass
-
-
-@app.route("/person_data/<int:patient_id>/save")
-def update_person(patient_id: PatientId):
-    #ToDo: implement
-    pass
-
-
-@app.route("/result/<int:patient_id>/pims")
-def result_pims(patient_id: PatientId):
-    #ToDo: implement
-    pass
-
-
-@app.route("/result/<int:patient_id>/kawasaki")
-def result_kawasaki(patient_id: int):
-    #ToDo: implement
-    pass
+@app.route("/reset_db")
+def reset_db():
+    if controller.reset_db():
+        flash("Database reset successful.", FlashMessageTypes.SUCCESS.value)
+    else:
+        flash("Database reset failed.", FlashMessageTypes.FAILURE.value)
+    return redirect(url_for("main"))
 
 
 if __name__ == "__main__":
