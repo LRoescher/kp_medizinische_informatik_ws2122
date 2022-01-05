f645c3534d5df59b951544216c17db218973453a
LRoescher <s5602199@msx.tu-dresden.de>
2022-01-05 16:59:15 +0100

    insert real data

--- Frontend/blueprint_results.py
+++ Frontend/blueprint_results.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from Backend.interface import PatientId, Interface, DecisionReasons, Disease
+from Backend.interface import PatientId, Interface, DecisionReasons, Disease, PatientData
 from Backend.example_interface import Example
 
 controller: Interface = Example()
@@ -18,13 +18,17 @@ def get_analysis():
 def result_pims(patient_id: PatientId):
     return render_template("result.html",
                            patient_id=patient_id,
-                           patient_data=controller.get_decision_reason(patient_id, Disease.PIMS),
-                           annotations=DecisionReasons.__annotations__)
+                           decision_reason_data=controller.get_decision_reason(patient_id, Disease.PIMS),
+                           decision_reason_annotations=DecisionReasons.__annotations__,
+                           patient_data=controller.get_patient_data(patient_id),
+                           annotations=PatientData.__annotations__)
 
 
 @results.route("/kawasaki/<int:patient_id>")
 def result_kawasaki(patient_id: PatientId):
     return render_template("result.html",
                            patient_id=patient_id,
-                           patient_data=controller.get_decision_reason(patient_id, Disease.KAWASAKI),
-                           annotations=DecisionReasons.__annotations__)
+                           decision_reason_data=controller.get_decision_reason(patient_id, Disease.KAWASAKI),
+                           decision_reason_annotations=DecisionReasons.__annotations__,
+                           patient_data = controller.get_patient_data(patient_id),
+                           annotations = PatientData.__annotations__)
